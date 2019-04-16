1721c2dcfc70ca03326cac3371f8ad8f86d5e581
Steven <hgong@woolworths.com.au>
2019-04-16 16:29:08 +1000

    place middleware in main reducer if need to add some middleware

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,6 +2,7 @@ import userReducer from "./user";
 import basketReducer from "./basket";
 
 export const reducers = ({ user, basket }, action) => ({
+    // middleware goes here, i.e calling analytics service, etc.
   user: userReducer(user, action),
   basket: basketReducer(basket, action)
 });
