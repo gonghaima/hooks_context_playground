f8976790ff2508615b71b66d1f745dcbd8b64af9
Steven <gonghaima@hotmail.com>
2019-04-16 19:55:53 +1000

    update welcome message

--- src/App.js
+++ src/App.js
@@ -21,9 +21,7 @@ class App extends Component {
         <div className="App">
           <header className="App-header">
             <img src={logo} className="App-logo" alt="logo" />
-            <p>
-              Edit <code>src/App.js</code> and save to reload.
-            </p>
+            <p>Welcome to context/hooks world</p>
             <a
               className="App-link"
               href="https://reactjs.org"
