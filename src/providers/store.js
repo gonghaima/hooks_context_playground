ae36a3ec91ae4c6f24750f86cb249343a921fc79
Steven <hgong@woolworths.com.au>
2019-04-16 13:47:44 +1000

    add Store.js - it's the core of global state management, when using context/hook

---
+++ src/providers/store.js
@@ -0,0 +1,11 @@
+import React, { createContext, useContext, useReducer } from "react";
+
+export const StateContext = createContext();
+
+export const StateProvider = ({ reducer, initialState, children }) => (
+  <StateProvider.Provider value={useReducer(reducer, initialState)}>
+    {children}
+  </StateProvider.Provider>
+);
+
+export const useStateValue = () => useContext(StateContext);
